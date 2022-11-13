fb3986bea78db24ea0e9eb1d6657944e0f0dc595
jeevan <jeevansai.makam@gmail.com>
2022-11-13 23:17:58 +0000

    check this

--- JS/main.js
+++ JS/main.js
@@ -29,5 +29,68 @@
         $('.equipmentdetails').empty();
         $('.equipmentdetails').append(equipment);
     }
+    function Clear(){
+        document.getElementById("txtname").value = "";
+        document.getElementById("txtemail").value = "";
+        document.getElementById("txtpnum").value = "";
+    }
+
+    function Reserve() {
+        if (isform_validate()) {
+            var formData = readFormData();
+            insertNewRecord(formData);
+            Clear();
+        }
+    }
+    // read the input form
+function readFormData() {
+    var formData = {};
+    formData["txtname"] = document.getElementById("txtname").value;
+    formData["txtemail"] = document.getElementById("txtemail").value;
+    formData["txtpnum"] = document.getElementById("txtpnum").value;
+    formData["equipmentid"] =equipmentid;
+    formData["title"] =equipmentdata.filter(x => x.id === equipmentid)[0].title;
+
+    return formData;
+}
+    // this function is  to validate the submit form
+function isform_validate(){
+    isValid = true;
+    var txtname = document.getElementById("txtname");
+    var txtemail = document.getElementById("txtemail");
+    var txtpnum = document.getElementById("txtpnum");
+    var emailvaild =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var phoneno = /^\d{10}$/;   
+    if (txtname.value == "" || txtname.value.length >=12 || txtname.value.length <= 3 ) {
+        if(txtname.value == ""){
+          alert("*name should not be empty");}
+        else{
+          alert("*name should be min 3 or max 12 words");
+        }
+        isValid = false;
+    }
+    else if (emailvaild.test(txtemail.value) == false)
+    {
+    alert("*Invalid email id");
+    isValid = false;
+    }
+    else if (!(txtpnum.value.match(phoneno))) {
+        alert("*must be 10 digit number");
+        isValid = false;
+    }
+    else{
+    isValid = true;
+    return isValid;}
+}
+function insertNewRecord(data) {
+    var table = document.getElementById("tblrecords").getElementsByTagName('tbody')[0];
+    // var newRow = table.insertRow(table.length);
+    var newRow = table.insertRow(0);
+    var records = [data.txtname,data.txtemail,data.txtpnum,data.title];
+    for (var i=0;i<records.length;i++){
+        cell = newRow.insertCell(i);
+        cell.innerHTML = records[i];
+    }
+}
 
     
