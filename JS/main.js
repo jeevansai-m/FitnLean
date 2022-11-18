158afb37cdf794ba5bc7bd0a83476dfbda8159a7
Meena Rao <meenaraokallepu24@gmail.com>
2022-11-18 12:06:03 +0000

    video adjusting

--- JS/main.js
+++ JS/main.js
@@ -93,5 +93,11 @@ function insertNewRecord(data) {
         cell.innerHTML = records[i];
     }
 }
+// navgation bar 
+const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links');
+toggleButton.addEventListener('click',function() {
+    for(var i=0; i<navbarLinks.length; i++)
+    navbarLinks[i].classList.toggle('active');
+});
 
-    
